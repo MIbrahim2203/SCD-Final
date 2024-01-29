11eb0a0e92f6ca5ce2b5e699809af66d8f2c02ab
Muhammad Ibrahim Zarar <157349270+MIbrahim2203@users.noreply.github.com>
2024-01-29 11:59:57 +0500

    Add files via upload
    Mian File

---
+++ SCD.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class HotelRoom
+{
+    void SearchModule();
+    //Function Done
+    void HotelDetailsModule();
+    void BookingModule();
+    void Availability();
+};
+
+int main()
+{
+    
+}
